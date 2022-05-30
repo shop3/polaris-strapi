5e8679a4516ac815ede9a603131c1cf207c5cd14
Giacomo Minighin <giacomo.minighin@gmail.com>
2022-05-30 18:36:39 +0200

    feat(edit): added getFiles method

---
+++ src/StrapiEdit/media/getFiles.ts
@@ -0,0 +1,24 @@
+import qs from 'qs';
+import { DbFile } from './types';
+
+async function getFiles(id: number | string): Promise<DbFile>;
+async function getFiles(ids: number[] | string[]): Promise<DbFile[]>;
+async function getFiles(input: number | string | number[] | string[]): Promise<DbFile | DbFile[]> {
+  const response = await fetch(
+    `/api/upload/files?${qs.stringify({
+      filters: { id: { $in: Array.isArray(input) ? input : [input] } },
+    })}`
+  );
+  if (response.status >= 400) {
+    const result = await response.json().catch(() => undefined);
+    throw new Error(`Failed to get files: ${result ? JSON.stringify(result.error, null, 2) : 'unknown error'}`);
+  }
+  const result = await response.json();
+  if (Array.isArray(input)) {
+    return result.results;
+  } else {
+    return result.results[0];
+  }
+}
+
+export default getFiles;
