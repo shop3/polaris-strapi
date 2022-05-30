f1afa4ed2a33279786efaa43bb548d40d7a11f4b
Giacomo Minighin <giacomo.minighin@gmail.com>
2022-05-30 18:36:01 +0200

    feat(edit): added media type guards

--- src/StrapiEdit/media/types.ts
+++ src/StrapiEdit/media/types.ts
@@ -1,8 +1,39 @@
 export type DbFile = {
   id: number;
-  attributes: any;
+  name: string;
+  mime: string;
+  size: number;
+  url: string;
+  ext: string;
+  [k: string]: any;
 };
 
+export function isFile(value: any): value is File {
+  return value instanceof File;
+}
+
 export function isFileArray(value: any): value is File[] {
-  return Array.isArray(value) && value.every((v) => v instanceof File);
+  return Array.isArray(value) && value.every(isFile);
+}
+
+export function isId(value: any): value is number | string {
+  return ['number', 'string'].includes(typeof value);
+}
+
+export function isIdArray(value: any): value is number[] | string[] {
+  return Array.isArray(value) && value.every(isId);
+}
+
+export function isFileDb(value: any): value is DbFile {
+  return (
+    typeof value.id === 'number' &&
+    typeof value.name === 'string' &&
+    typeof value.size === 'number' &&
+    typeof value.mime === 'string' &&
+    typeof value.url === 'string'
+  );
+}
+
+export function isFileDbArray(value: any): value is DbFile[] {
+  return Array.isArray(value) && value.every(isFileDb);
 }
