298d734c0834072374e4bdba73ba73395a829057
Giacomo Minighin <giacomo.minighin@gmail.com>
2022-05-31 13:48:48 +0200

    chore(edit): exported fetchFiles method

--- src/StrapiEdit/media/index.ts
+++ src/StrapiEdit/media/index.ts
@@ -1,3 +1,4 @@
 export { default as StrapiMediaInput } from './StrapiMediaInput';
+export { default as fetchFiles } from './fetchFiles';
 export { default as uploadFiles } from './uploadFiles';
 export * from './types';
