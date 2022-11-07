9eb391a7c8fd2080fa4d438b9ed87347108b2970
Giacomo Minighin <giacomo.minighin@gmail.com>
2022-11-08 00:56:54 +0100

    chore: storybook build with webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,9 @@ module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   staticDirs: ['./public'],
+  core: {
+    builder: 'webpack5',
+  },
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.mjs$/,
